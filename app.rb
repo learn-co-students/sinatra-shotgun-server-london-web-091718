6da486acf44a84c270fbb4b45f67ed0c77567628
Patrick <ctrlaltpat@gmail.com>
2018-10-08 11:42:27 +0100

    Done.

--- app.rb
+++ app.rb
@@ -3,7 +3,8 @@ require_relative 'config/environment'
 class App < Sinatra::Base
 
   get '/' do 
-    "Welcome to your app!!!! "
+    "Welcome to your app!!!! I built this!!" +
+    "\nHaving to use bundle exec is annoying!"
   end
 
 end
